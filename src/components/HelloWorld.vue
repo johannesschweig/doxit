a91745af4055362de521911ac2fd901db5a5cbe8
Johannes Schweig <johannes.schweig@valsight.com>
2020-04-02 09:25:16 +0200

    initial working

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,19 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress" target="_blank" rel="noopener">e2e-cypress</a></li>
+    <h1>Doxit</h1>
+<button @click="disconnect" v-if="status === 'connected'">Disconnect</button>
+  <button @click="connect" v-if="status === 'disconnected'">Connect</button> {{ status }}
+  <br /><br />
+  <div v-if="status === 'connected'">
+    <form @submit.prevent="sendMessage" action="#">
+      <input v-model="message"><button type="submit">Send Message</button>
+    </form>
+    <ul id="logs">
+      <li v-for="log in logs" class="log">
+        {{ log.event }}: {{ log.data }}
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  </div>
   </div>
 </template>
 
@@ -39,24 +22,44 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      message: "",
+      logs: [],
+      status: "disconnected",
+      ws: 'ws://localhost:8081'//"wss://echo.websocket.org"
+    }
+  },
+  methods: {
+    connect() {
+      this.socket = new WebSocket(this.ws);
+      this.socket.onopen = () => {
+        this.status = "connected";
+        this.logs.push({ event: "Connected to", data: this.ws})
+        
+
+        this.socket.onmessage = ({data}) => {
+          this.logs.push({ event: "Recieved message", data });
+        };
+      };
+    },
+    disconnect() {
+      this.socket.close();
+      this.status = "disconnected";
+      this.logs = [];
+    },
+    sendMessage(e) {
+      this.socket.send(this.message);
+      this.logs.push({ event: "Sent message", data: this.message });
+      this.message = "";
+    }
+  },
+  created() {
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
